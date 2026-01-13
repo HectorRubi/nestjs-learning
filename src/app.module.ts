d023e9ca4c1aca7ee509a336967f95f406d39394
Hector Rubi <hector.rubi.garcia@outlook.com>
2026-01-12 21:45:32 -0600

    feat: configure TypeORM for PostgreSQL database connection according to both TypeORM and NestJS documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '',
+      port: 0,
+      username: '',
+      password: '',
+      database: '',
+      entities: [],
+      synchronize: true,
+    }),
     CatsModule,
   ],
   controllers: [AppController],

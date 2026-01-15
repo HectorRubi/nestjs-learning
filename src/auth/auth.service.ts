ab9248f30a12ba0158d4c8030245b08bc60830da
Hector Rubi <hector.rubi.garcia@outlook.com>
2026-01-14 21:05:25 -0600

    feat: implement authentication controller and service with login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  signIn(username: string, pass: string) {
+    const user = this.usersService.findOne(username);
+
+    if (!user || user.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    // TODO: Generate and return JWT or session info
+    return { id: user.id, username: user.name };
+  }
+}
